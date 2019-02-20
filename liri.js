ea4fe6590b75a258ca969c065d8530dccb808977
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-02-19 19:54:34 -0800

    working on concert-this

--- liri.js
+++ liri.js
@@ -2,23 +2,23 @@
 var spotifynode = require("Node-Spotify-API");
 var axios = require("axios");
 var moment = require("moment");
-var dotenv =  require ("DotEnv");
+var dotenv =  require("DotEnv");
 require("dotenv").config();
 
 // spotify keys
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
-function concert-this(process.argv[3]) {
-    var artist = process.argv[3];
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+var searchterm = process.argv.slice(2).join("");
+
+function concert-this(searchterm) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + searchterm + "/events?app_id=codingbootcamp";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     })
         .then(function(response) {
-            console.log(queryURL);
             var results = response.root;
 
             for (var i = 0; i<results.length; i++) {
@@ -26,19 +26,20 @@ function concert-this(process.argv[3]) {
                 var location = results[i].venue.city + ", " + results[i].venue.country;
                 var date = moment(results[i].venue.datetime, "MM/DD/YYYY");
 
-                console.log(artist, "\n", venue, "\n", location, "\n", date, "\n\n");
+                console.log(artist, "\n", venue, "\n", location, "\n", date, "\n\n\n");
             }
 }
 
-function spotify-this-song(song) {
+function spotify-this-song(searchterm) {
 
 }
 
-function movie-this(movie) {
+function movie-this(searchterm) {
+    var queryURL = "http://www.omdbapi.com/?apikey=trilogy&" + ;
 
 }
 
-function do-what-it-says(random) {
+function do-what-it-says(searchterm) {
 
 }
 
