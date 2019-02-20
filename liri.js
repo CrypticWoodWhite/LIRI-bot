18e93adde570b3f1fe3b4b255106697bfbe04c25
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-02-19 21:00:59 -0800

    concert this

--- liri.js
+++ liri.js
@@ -2,9 +2,8 @@
 var spotifynode = require("Node-Spotify-API");
 var axios = require("axios");
 var moment = require("moment");
-var dotenv =  require("DotEnv");
+var dotenv =  require("dotenv").config();
 var request = require("request");
-require("dotenv").config();
 
 // spotify keys
 var keys = require("./keys.js");
@@ -12,35 +11,42 @@ var spotify = new Spotify(keys.spotify);
 
 var searchterm = process.argv.slice(2).join("");
 
-function concert-this(searchterm) {
-    var queryURL = "https://rest.bandsintown.com/artists/" + searchterm + "/events?app_id=codingbootcamp";
+function concert-this(error, searchterm) {
+    if (error) {
+        console.log("ERROR: " + error);
+    }
+    else {
+        var queryURL = "https://rest.bandsintown.com/artists/" + searchterm + "/events?app_id=codingbootcamp";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"  
-    })
-        .then(function(response) {
-            var results = response.root;
+        $.ajax({
+            url: queryURL,
+            method: "GET"  
+        })
+            .then(function(response) {
+                var results = response.root;
 
-            for (var i = 0; i<results.length; i++) {
-                var venue = results[i].venue.name;
-                var location = results[i].venue.city + ", " + results[i].venue.country;
-                var date = moment(results[i].venue.datetime, "MM/DD/YYYY");
+                for (var i = 0; i<results.length; i++) {
+                    var venue = results[i].venue.name;
+                    var location = results[i].venue.city + ", " + results[i].venue.country;
+                    var date = moment(results[i].venue.datetime, "MM/DD/YYYY");
 
-                console.log(artist, "\n", venue, "\n", location, "\n", date, "\n\n\n");
-            }
+                    console.log(artist, "\n", venue, "\n", location, "\n", date, "\n\n\n");
+                }
+            })
+    }
 }
+    
 
-function spotify-this-song(searchterm) {
+// function spotify-this-song(searchterm) {
 
-}
+// }
 
-function movie-this(searchterm) {
-    var queryURL = "http://www.omdbapi.com/?apikey=trilogy&" + ;
+// function movie-this(searchterm) {
+//     var queryURL = "http://www.omdbapi.com/?apikey=trilogy&" + ;
 
-}
+// }
 
-function do-what-it-says(searchterm) {
+// function do-what-it-says(searchterm) {
 
-}
+// }
 
