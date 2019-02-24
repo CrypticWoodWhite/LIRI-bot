cf4944d25c983fb9c785efb2c46b79ff904fe0ce
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-02-23 16:06:33 -0800

    2/23/19

--- liri.js
+++ liri.js
@@ -1,52 +1,86 @@
 // add all node packages
-var spotifynode = require("Node-Spotify-API");
+var Spotify = require("Node-Spotify-API");
 var axios = require("axios");
 var moment = require("moment");
-var dotenv =  require("dotenv").config();
-var request = require("request");
+require("dotenv").config();
 
 // spotify keys
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
-var searchterm = process.argv.slice(2).join("");
+// taking in user input
+var functioncalled = process.argv[2];
+var searchterm = process.argv.slice(3).join("");
 
-function concert-this(error, searchterm) {
-    if (error) {
-        console.log("ERROR: " + error);
-    }
-    else {
-        var queryURL = "https://rest.bandsintown.com/artists/" + searchterm + "/events?app_id=codingbootcamp";
+// instructions
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"  
+function concert_this(searchterm) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + searchterm + "/events?app_id=codingbootcamp";
+    // console.log(searchterm);
+    // console.log(queryURL);
+
+    axios.get(queryURL)
+        .then(function(response) {
+            // console.log(response);
+            // for loop is not working....
+            for (var i=0; i<response.length; i++) {
+                var venue = response[i].venue.name;
+                var location = response[i].venue.city + ", " + response[i].venue.country;
+                var date = moment(response[i].venue.datetime, "MM/DD/YYYY");
+                console.log("You searched for: " + searchterm + "\n"+ venue + "\n" + location + "\n" + date + "\n\n");
+            }
         })
-            .then(function(response) {
-                var results = response.root;
+        .catch(function(err) {
+            console.log("ERROR: " + err);
+        })
+}
 
-                for (var i = 0; i<results.length; i++) {
-                    var venue = results[i].venue.name;
-                    var location = results[i].venue.city + ", " + results[i].venue.country;
-                    var date = moment(results[i].venue.datetime, "MM/DD/YYYY");
-
-                    console.log(artist, "\n", venue, "\n", location, "\n", date, "\n\n\n");
-                }
-            })
-    }
+if (functioncalled === "concert_this") {
+    concert_this(searchterm);
+}
+else if (functioncalled === "spotify_this_song") {
+    spotify_this_song(searchterm);
+}
+else if (functioncalled === "movie_this") {
+    movie_this(searchterm);
+}
+else if (functioncalled === "do_what_it_says") {
+    do_what_it_says(searchterm);
+}
+else {
+    console.log("\nDid you make a typo? Try again!");
+    console.log("These are the functions you can use:");
+    console.log("- concert_this");
+    console.log("- spotify_this_song");
+    console.log("- movie_this");
+    console.log("- do_what_it_says\n");
 }
     
 
-// function spotify-this-song(searchterm) {
+function spotify_this_song(searchterm) {
+    if (searchterm.length === 0) {
+        console.log("You did not give a song so we're using 'The Sign' by Ace of Base\n");
+        searchterm = "The Sign";
+    }
+    spotify.search({type: "track", query: searchterm})
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(err) {
+            console.log("ERROR: " + err);
+        })
+}
+
+// function movie_this(searchterm) {
+
+    // var queryURL = "http://www.omdbapi.com/?apikey=trilogy&" + stufffffff ;
 
 // }
 
-// function movie-this(searchterm) {
-//     var queryURL = "http://www.omdbapi.com/?apikey=trilogy&" + ;
+// function do_what_it_says(searchterm) {
 
 // }
 
-// function do-what-it-says(searchterm) {
 
-// }
+
 
